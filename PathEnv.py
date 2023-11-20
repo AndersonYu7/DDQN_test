642739671d0f2a2f93b7d12b0b026de3491ffdbc
AndersonYu7 <andersonyu614@gmail.com>
2023-11-20 21:11:01 +0800

    update code

--- PathEnv.py
+++ PathEnv.py
@@ -1,11 +1,14 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+from checkpath import Checking
 
-class MazeEnv:
+class MazeEnv():
     def __init__(self, size, num_obstacles):
         self.size = size
         self.num_obstacles = num_obstacles
+        self.episode_ended = False
+        self.obstacles = []
 
         #生成起點與終點
         self.state_space = np.zeros((size, size))
@@ -18,70 +21,131 @@ class MazeEnv:
         self.actions = ['up', 'down', 'left', 'right']
         self.state_space[self.start] = 1  # mark the start
         self.state_space[self.goal] = 2  # mark the goal
-
-        #生成障礙物
-        self.obstacles = []
-        while(len(self.obstacles)<num_obstacles):
-            obstacle = self.generate_random_position()
-            if(obstacle!=self.start and obstacle!=self.goal and obstacle not in self.obstacles):
-                self.obstacles.append(obstacle)
-
-        print(len(self.obstacles))
-        for obstacles in self.obstacles:
-            self.state_space[obstacles] = -1
+        self.generate_random_obstacles()
+        
 
     def generate_random_position(self):
         position = np.random.randint(0, self.size), np.random.randint(0, self.size)
         return position    
 
-    # def generate_path(self):
-    #     visited = np.zeros((self.size, self.size), dtype=bool)
-    #     stack = [self.start]
-    #     visited[self.start] = True
+    def generate_random_obstacles(self):
+        start = self.start[0]*self.size+self.start[1]
+        end = self.goal[0]*self.size+self.goal[1]
+        graph = np.zeros((self.size, self.size))
+        graph[self.start] = 1
+        graph[self.goal] = 2
+        
+        obstacles = np.random.choice(range(self.size*self.size), size=self.num_obstacles, replace=False).tolist()
+        for pos in obstacles:
+            graph[(int(pos/self.size), int(pos%self.size))] = -1
 
-    #     while stack:
-    #         current = stack[-1]
-    #         neighbors = self.get_neighbors(current)
-    #         unvisited_neighbors = [neighbor for neighbor in neighbors if not visited[neighbor]]
-    #         if unvisited_neighbors:
-    #             next_cell = random.choice(unvisited_neighbors)
-    #             self.state_space[next_cell] = -1  # mark the path
-    #             visited[next_cell] = True
-    #             stack.append(next_cell)
-    #         else:
-    #             stack.pop()
+        while(start in obstacles or end in obstacles or not Checking(graph, self.start, self.goal)):
+            obstacles = np.random.choice(range(self.size*self.size), size=self.num_obstacles, replace=False).tolist()
+            graph = np.zeros((self.size, self.size))
+            graph[self.start] = 1
+            graph[self.goal] = 2
+            for pos in obstacles:
+                graph[(int(pos/self.size), int(pos%self.size))] = -1
 
-    # def get_neighbors(self, cell):
-    #     neighbors = []
-    #     for action in self.actions:
-    #         neighbor = self.get_next_position(cell, action)
-    #         if 0 <= neighbor[0] < self.size and 0 <= neighbor[1] < self.size:
-    #             neighbors.append(neighbor)
-    #     return neighbors
+        self.obstacles = obstacles
+        for pos in obstacles:
+            self.state_space[(int(pos/self.size), int(pos%self.size))] = -1
 
     def get_next_position(self, current_position, action):
         if action == 'up':
-            # return (current_position[0] - 1, current_position[1])
             return max(0, current_position[0] - 1), current_position[1]
         elif action == 'down':
-            # return (current_position[0] + 1, current_position[1])
-            return min(self.size - 1, current_position+[0]+1), current_position[1]
+            return min(self.size - 1, current_position[0]+1), current_position[1]
         elif action == 'left':
-            # return (current_position[0], current_position[1] - 1)
             return current_position[0], max(0, current_position[1] - 1)
         elif action == 'right':
-            # return (current_position[0], current_position[1] + 1)
             return current_position[0], min(self.size - 1, current_position[1] + 1)
         else:
             return ValueError("Invalid action")
 
-    def reset(self):
+    def hit_reset(self):    #僅僅初始化起終點
+        self.state_space = np.zeros((self.size, self.size))
         self.current_position = self.start
-        return np.copy(self.state_space)
+        self.state_space[self.start] = 1
+        self.state_space[self.goal] = 2
+        for pos in self.obstacles:
+            self.state_space[(int(pos/self.size), int(pos%self.size))] = -1
+
+        return self.state_space
+
+    def reset(self):       #新的起終點
+        # self.state_space = np.zeros((self.size, self.size))
+        graph = np.zeros((self.size, self.size))
+
+        #重建障礙物
+        for pos in self.obstacles:
+            graph[(int(pos/self.size), int(pos%self.size))] = -1
+
+        #新的初始點
+        start = self.generate_random_position()
+        while(start in self.obstacles):
+            start = self.generate_random_position()
+
+        #新的結束點
+        goal = self.generate_random_position()
+        while(goal in self.obstacles or goal == start):
+            goal = self.generate_random_position()
+
+        graph[start] = 1
+        graph[goal] = 2
+
+        cnt = 0 #防止搜尋不到 假如搜尋不到 反回原本的圖
+        while(not Checking(graph, start, goal)):
+            cnt+=1
+            if(cnt>20):
+                break
+            graph[start] = 0
+            graph[goal] = 0
+
+            #新的初始點
+            start = self.generate_random_position()
+            while(start in self.obstacles):
+                start = self.generate_random_position()
+
+            #新的結束點
+            goal = self.generate_random_position()
+            while(goal in self.obstacles or goal == start):
+                goal = self.generate_random_position()
+
+            graph[start] = 1
+            graph[goal] = 2
+
+        if(cnt>20):
+            self.current_position = self.start
+            return self.state_space
+        else:
+            self.current_position = start
+            self.state_space = graph
+            return graph
 
     def step(self, action):
+        #return state, reward, done, hit
+
         # Implement step logic
-        pass
+        if self.episode_ended:
+            return self.reset(), 0, False, False
+
+        new_position = self.get_next_position(self.current_position, action)
+
+        if(new_position == self.current_position):
+            return np.copy(self.state_space), -1, True, True
+        
+        elif(new_position in self.obstacles):
+            return np.copy(self.state_space), -1, True, True
+
+        elif(new_position == self.goal):
+            self._episode_ended = True
+            return np.copy(self.state_space), 1, True, False
+
+        else:
+            return np.copy(self.state_space), 0, False, False
+        
+        
 
     def render(self):
         plt.imshow(self.state_space, cmap='gray')
@@ -90,10 +154,22 @@ class MazeEnv:
 
 # Example usage
 maze_size = 10
-num_obstacles = 10
+num_obstacles = 20
 env = MazeEnv(size=maze_size, num_obstacles=num_obstacles)
+# env.reset()
+# print(env.state_space)
+# print(Checking(env.state_space, env.start, env.goal))
+# env.render()
+# print(env.current_position)
+# env.step('right')
+# env.render()
+
+env.hit_reset()
+env.render()
+env.hit_reset()
+env.render()
+
 env.reset()
-print(env.state_space)
 env.render()
 
 # Now you have a maze with a path from start to goal, and you can add logic to randomly generate other positions.
--- checkpath.py
+++ checkpath.py
@@ -1,39 +1,43 @@
 import numpy as np
 
 def Valid(i,limit,flag,value) :
-	if i>=0 and i<=limit-1 and value[i] == 255 and flag[i] == 0 :
+	if i>=0 and i<=limit-1 and (value[i] == 0 or value[i] == 2) and flag[i] == 0 :
 		return True 
 	return False	
 
-def Checking(graph) :
+def Checking(graph, start, end) :
 	stack = []
 	x,y = graph.shape
 	graph1 = np.reshape(graph,-1)
 	limit = len(graph1)
 	flag = np.zeros([len(graph1)])
 
-	stack.append(0)
-	flag[0] = 1
-	while (len(stack)>0 and not flag[limit-1] == 1) :
+	stack.append(start[0]*x+start[1])
+	flag[start[0]*x+start[1]] = 1
+	while (len(stack)>0 and not flag[end[0]*x+end[1]] == 1) :
 		element = stack.pop()
 		#print element
-		'''
+		
 		if Valid(element-1,limit,flag,graph1) and not element%x == 0 :
 			stack.append(element-1)
 			flag[element-1] = 1
-		'''	
+		
 		if Valid(element+1,limit,flag,graph1) and not (element+1)%x == 0:
 			stack.append(element+1)
 			flag[element+1] = 1
-		'''	
+		
 		if Valid(element-x,limit,flag,graph1) :
 			stack.append(element-x)
 			flag[element-x] = 1
-		'''	
+		
 		if Valid(element+x,limit,flag,graph1) :
 			stack.append(element+x)
 			flag[element+x] = 1
 
-	if flag[limit-1] == 1 :
+	# print(start, end)
+	# print(start[0]*x+start[1])
+	# print(end[0]*x+end[1])
+
+	if flag[end[0]*x+end[1]] == 1 :
 		return True
 	return False	
